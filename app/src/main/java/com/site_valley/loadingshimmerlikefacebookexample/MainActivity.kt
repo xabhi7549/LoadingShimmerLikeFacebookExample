0165f366161213648a08d52a8f488a72464a7d78
Abhinav <xabhi7549@gmail.com>
2021-04-12 17:50:41 +0530

    Initial commit

---
+++ app/src/main/java/com/site_valley/loadingshimmerlikefacebookexample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.site_valley.loadingshimmerlikefacebookexample
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.shimmer.ShimmerFrameLayout
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(mainLooper).postDelayed({
+            findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container).stopShimmer()
+            findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container).visibility = View.GONE
+            findViewById<TextView>(R.id.text).visibility = View.GONE
+
+        }, 5000)
+    }
+}
+
